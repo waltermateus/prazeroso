bc9caca6256dc17f4cf6617d6e039ab3c52f2fff
walter mateus <walter@CyberKannibal.CyberKa>
2024-08-22 17:41:44 -0400

    first commit

--- whatsapp.js
+++ whatsapp.js
@@ -1,7 +1,7 @@
 function sendToWhatsapp(event) {
   event.preventDefault(); // Prevent the form from submitting normally
 
-  let number = "244931516341"; // Remove the "+" from the number
+  let number = "+244931516341";
   let fullName = document.querySelector('input[name="full_name"]').value;
   let phone = document.querySelector('input[name="email_address"][type="number"]').value;
   let email = document.querySelector('input[name="email_address"][type="email"]').value;
@@ -10,7 +10,8 @@ function sendToWhatsapp(event) {
   let bookingDate = document.querySelector('input[name="booking_date"]').value;
   let message = document.querySelector('textarea[name="message"]').value;
 
-  let text = "Nome: " + fullName + "%0a"
+  var url = "whatsapp://send?phone=" + number + "&text="
+    + "Nome: " + fullName + "%0a"
     + "Telefone: " + phone + "%0a"
     + "Email: " + email + "%0a"
     + "Morada: " + address + "%0a"
@@ -18,32 +19,11 @@ function sendToWhatsapp(event) {
     + "Data de Reserva: " + bookingDate + "%0a"
     + "Mensagem: " + message + "%0a%0a";
 
-  let url;
-  
-  // Check if the device is iOS
-  if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-    // iOS URL Scheme
-    url = "whatsapp://send?phone=" + number + "&text=" + encodeURIComponent(text);
-  } else if (/Android/i.test(navigator.userAgent)) {
-    // Android Intent URL
-    url = "intent://send/#Intent;scheme=smsto;package=com.whatsapp;i.action=android.intent.action.SENDTO;i.phone=" + number + ";S.msg=" + encodeURIComponent(text) + ";end";
-  } else {
-    // Web URL for desktop or unsupported mobile browsers
-    url = "https://web.whatsapp.com/send?phone=" + number + "&text=" + encodeURIComponent(text);
-  }
-
-  // Try to open the URL
-  window.location.href = url;
-
-  // Fallback for desktop if the WhatsApp web doesn't open
-  setTimeout(function() {
-    if (!/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-      window.location.href = "https://web.whatsapp.com/send?phone=" + number + "&text=" + encodeURIComponent(text);
-    }
-  }, 1000);
+  window.open(url, '_blank').focus();
 }
 
 // Add event listener to the form
+document.querySelector('.footer-form').addEventListener('submit', sendToWhatsapp);
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.footer-form').addEventListener('submit', sendToWhatsapp);
 });
